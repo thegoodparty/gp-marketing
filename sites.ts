a672ec653d699e1523723caf5caebbaf90a912a2
Andy Parsons <andparsons@gmail.com>
2025-11-14 10:49:35 +0000

    Add VERCEL_URL as first check

--- sites.ts
+++ sites.ts
@@ -6,6 +6,6 @@ export const sites = {
 		icon: Logo,
 		url: process.env.NODE_ENV === 'development'
 			? 'http://localhost:3009'
-			: process.env.NEXT_PUBLIC_PREVIEW_URL || 'https://gp-marketing-git-develop-good-party.vercel.app',
+			: process.env.VERCEL_URL || process.env.NEXT_PUBLIC_PREVIEW_URL || 'https://gp-marketing-git-develop-good-party.vercel.app',
 	},
 };

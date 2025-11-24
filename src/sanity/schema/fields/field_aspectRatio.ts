05fafd6f55befbc243066b2c62dd10a3c2abdba8
Victoria Martynenko <dev@evensix.com>
2025-11-24 10:44:43 +0000

    sanity fields update: added lists to block_glossaryTermDefinition, aspectRatio to article image, lastUpdated to article

---
+++ src/sanity/schema/fields/field_aspectRatio.ts
@@ -0,0 +1,54 @@
+export const field_aspectRatio = {
+	name: 'field_aspectRatio',
+	title: 'Aspect Ratio',
+	description: 'Defines the aspect ratio for this asset.',
+	options: {
+		collapsible: false,
+		list: [
+			{
+				_key: 'c283a86d9052',
+				_type: 'item',
+				title: '1:1',
+				value: '1:1',
+			},
+			{
+				_key: 'b47851e38fd6',
+				_type: 'item',
+				title: '16:9',
+				value: '16:9',
+			},
+			{
+				_key: 'a53c9894abbd',
+				_type: 'item',
+				title: '9:16',
+				value: '9:16',
+			},
+			{
+				_key: 'd26febae1afd',
+				_type: 'item',
+				title: '5:4',
+				value: '5:4',
+			},
+			{
+				_key: '8d033ca1dc56',
+				_type: 'item',
+				title: '4:5',
+				value: '4:5',
+			},
+			{
+				_key: '277de894f1ee',
+				_type: 'item',
+				title: '4:3',
+				value: '4:3',
+			},
+			{
+				_key: '63be72d6603c',
+				_type: 'item',
+				title: '3:4',
+				value: '3:4',
+			},
+		],
+	},
+	initialValue: 'Right',
+	type: 'string',
+};

f6dad5c72841f67df9c56906dee615d2c352d98e
Evensix <93653507+evensix-dev@users.noreply.github.com>
2025-11-25 21:27:35 +0000

    Rename variable from refinedPath to refinedUrl

--- sanity.config.ts
+++ sanity.config.ts
@@ -55,21 +55,21 @@ export default defineConfig({
 										origin: siteData.url,
 										draftMode: '/api/draft-mode/enable',
 										async preview(doc) {
-											let refinedPath = `${siteData.url}${String(path)}`;
+											let refinedUrl = `${siteData.url}${String(path)}`;
 											// If the type has dynamic parts of its URL, we'll loop over and replace the params here
 											if ('pathParams' in type.options && Object.keys(type.options.pathParams).length > 0) {
 												for (const [param, paramPath] of Object.entries(type.options.pathParams)) {
 													// If the path has a reference, we'll need to resolve the ref via a fetch at this point
 													if(String(paramPath).includes('->')) {
 														const refValue = await ctx.getClient({apiVersion: defaultApiVersion}).fetch<{value?: string}|null>(`*[_id == $id][0]{"value":${paramPath}}`,{id: doc?._id})
-														refinedPath = refinedPath.replaceAll(`:${param}`, refValue?.value || 'draft');
+														refinedUrl = refinedUrl.replaceAll(`:${param}`, refValue?.value || 'draft');
 													}
 													else {
-														refinedPath = refinedPath.replaceAll(`:${param}`, String(get(doc, String(paramPath))));
+														refinedUrl = refinedUrl.replaceAll(`:${param}`, String(get(doc, String(paramPath))));
 													}
 												}
 											}
-											return refinedPath;
+											return refinedUrl;
 										},
 									}
 								} satisfies IframeOptions),

790000a1d43a77a32675dc1aa721a72bfcb31b44
evensix-dev <dev@evensix.com>
2025-12-01 10:22:19 +0000

    fix(119): Testimonial block 2 cards tablet
    (cherry picked from commit e2f945fd8b7d229bafb1b90c5602061c98874c6d)

--- src/ui/TestimonialBlock.tsx
+++ src/ui/TestimonialBlock.tsx
@@ -44,17 +44,19 @@ export function TestimonialBlock(props: TestimonialBlockProps) {
 					{props.items && props.items.length > 0 && (
 						<div className='flex flex-wrap justify-center gap-6 lg:gap-8'>
 							{props.items?.map((item, index) => {
+								const isTwoItems = props.items.length === 2;
+
 								return (
 									<Testimonial
 										{...item}
 										key={index}
 										color={colors[index]}
-										className='grow-0 shrink-0
-    basis-full                       /* 1 col by default */
-    sm:basis-[calc((100%-1.5rem)/2)] /* 2 cols, gap-6 = 1.5rem, so (100% - 1*gap)/2 */
-    md:basis-[calc((100%-3rem)/3)]   /* 3 cols, two gaps of 1.5rem -> 3rem total */
-    lg:basis-[calc((100%-4rem)/3)]   /* 3 cols, two gaps of 2rem -> 4rem total */
-  '
+										className={`
+											grow-0 shrink-0 basis-full /* 1 col by default */
+											sm:basis-[calc((100%-1.5rem)/2)] /* 2 cols, gap-6 = 1.5rem, so (100% - 1*gap)/2 */
+											${isTwoItems ? 'md:basis-[calc((100%-3rem)/2)]' : 'md:basis-[calc((100%-3rem)/3)]'}  /* 2 or 3 cols, two gaps of 1.5rem -> 3rem total */
+											lg:basis-[calc((100%-4rem)/3)] /* 3 cols, two gaps of 2rem -> 4rem total */
+										`}
 									/>
 								);
 							})}

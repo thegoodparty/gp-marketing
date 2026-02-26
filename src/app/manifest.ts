a7e2dde8fb77213cbcd59a34350358c04b6213ca
Ernesto Herrera Salinas <ernestos@gracchusconsulting.com>
2026-02-26 08:16:53 -0600

    Refactor web app manifest icons for improved PWA support
    - Updated the manifest.ts file to include duplicate entries for web app icons with different purposes (any and maskable).
    - Ensured consistency in icon properties and formatting for better readability and maintainability.

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,19 +4,31 @@ export default function manifest(): MetadataRoute.Manifest {
 	return {
 		name: 'Boilerplate',
 		short_name: 'Boilerplate',
-		"icons": [
-		  {
-			"src": "/web-app-manifest-192x192.png",
-			"sizes": "192x192",
-			"type": "image/png",
-			"purpose": "maskable"
-		  },
-		  {
-			"src": "/web-app-manifest-512x512.png",
-			"sizes": "512x512",
-			"type": "image/png",
-			"purpose": "maskable"
-		  }
+		icons: [
+			{
+				src: '/web-app-manifest-192x192.png',
+				sizes: '192x192',
+				type: 'image/png',
+				purpose: 'any',
+			},
+			{
+				src: '/web-app-manifest-512x512.png',
+				sizes: '512x512',
+				type: 'image/png',
+				purpose: 'any',
+			},
+			{
+				src: '/web-app-manifest-192x192.png',
+				sizes: '192x192',
+				type: 'image/png',
+				purpose: 'maskable',
+			},
+			{
+				src: '/web-app-manifest-512x512.png',
+				sizes: '512x512',
+				type: 'image/png',
+				purpose: 'maskable',
+			},
 		],
 		start_url: '/',
 		display: 'standalone',

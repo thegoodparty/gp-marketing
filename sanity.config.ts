e1f9d87347b4d9317de0f5a6c57af0742aa14dee
Ernesto Herrera Salinas <ernestos@gracchusconsulting.com>
2026-06-29 07:51:27 -0600

    Refactor Sanity configuration for improved preview handling
    - Streamlined the logic for generating previews in the Sanity configuration.
    - Enhanced the handling of dynamic URL parameters within the preview function.
    - Ensured consistent structure for document views, maintaining clarity in the configuration.

--- sanity.config.ts
+++ sanity.config.ts
@@ -77,56 +77,56 @@ export default defineConfig({
 						} satisfies IframeOptions),
 				);
 			} else if (type.options && 'channels' in type.options && Object.keys(type.options.channels).length > 0) {
-					for (const [siteId, path] of Object.entries(type.options.channels)) {
-						const siteData = sites[siteId as keyof typeof sites];
-						previews.push(
-							S.view
-								.component(Iframe)
-								.title(siteData.title)
-								.id(siteId)
-								.options({
-									key: ctx.documentId,
-									reload: { button: true },
-									url: {
-										origin: siteData.url,
-										draftMode: '/api/draft-mode/enable',
-										async preview(doc) {
-											let refinedPath = String(path);
-											// If the type has dynamic parts of its URL, we'll loop over and replace the params here
-											if ('pathParams' in type.options && Object.keys(type.options.pathParams).length > 0) {
-												for (const [param, paramPath] of Object.entries(type.options.pathParams)) {
-													// If the path has a reference, we'll need to resolve the ref via a fetch at this point
-													if(String(paramPath).includes('->')) {
-														const refValue = await ctx.getClient({apiVersion: defaultApiVersion}).fetch<{value?: string}|null>(`*[_id == $id][0]{"value":${paramPath}}`,{id: doc?._id})
-														refinedPath = refinedPath.replaceAll(`:${param}`, refValue?.value || 'draft');
-													}
-													else {
-														refinedPath = refinedPath.replaceAll(`:${param}`, String(get(doc, String(paramPath))));
-													}
+				for (const [siteId, path] of Object.entries(type.options.channels)) {
+					const siteData = sites[siteId as keyof typeof sites];
+					previews.push(
+						S.view
+							.component(Iframe)
+							.title(siteData.title)
+							.id(siteId)
+							.options({
+								key: ctx.documentId,
+								reload: { button: true },
+								url: {
+									origin: siteData.url,
+									draftMode: '/api/draft-mode/enable',
+									async preview(doc) {
+										let refinedPath = String(path);
+										// If the type has dynamic parts of its URL, we'll loop over and replace the params here
+										if ('pathParams' in type.options && Object.keys(type.options.pathParams).length > 0) {
+											for (const [param, paramPath] of Object.entries(type.options.pathParams)) {
+												// If the path has a reference, we'll need to resolve the ref via a fetch at this point
+												if(String(paramPath).includes('->')) {
+													const refValue = await ctx.getClient({apiVersion: defaultApiVersion}).fetch<{value?: string}|null>(`*[_id == $id][0]{"value":${paramPath}}`,{id: doc?._id})
+													refinedPath = refinedPath.replaceAll(`:${param}`, refValue?.value || 'draft');
+												}
+												else {
+													refinedPath = refinedPath.replaceAll(`:${param}`, String(get(doc, String(paramPath))));
 												}
 											}
-											return refinedPath;
-										},
-									}
-								} satisfies IframeOptions),
-						);
-					}
+										}
+										return refinedPath;
+									},
+								}
+							} satisfies IframeOptions),
+					);
 				}
-				return S.document().views([
-					S.view.form(),
-					S.view
-						.component(DocumentsPane)
-						.title('Referenced by')
-						.options({
-							query: `*[references(select(string::startsWith($id,"drafts.") => string::split($id, "drafts.")[1], $id))]`,
-							params: { id: '_id' },
-							debug: false,
-							useDraft: true,
-							duplicate: false,
-							options: { perspective: 'previewDrafts', apiVersion: '2025-10-01' },
-						}),
-					...previews,
-				]);
+			}
+			return S.document().views([
+				S.view.form(),
+				S.view
+					.component(DocumentsPane)
+					.title('Referenced by')
+					.options({
+						query: `*[references(select(string::startsWith($id,"drafts.") => string::split($id, "drafts.")[1], $id))]`,
+						params: { id: '_id' },
+						debug: false,
+						useDraft: true,
+						duplicate: false,
+						options: { perspective: 'previewDrafts', apiVersion: '2025-10-01' },
+					}),
+				...previews,
+			]);
 			},
 			structure: defaultStructure,
 		}),
